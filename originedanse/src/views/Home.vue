c5d3c5889d2a5e95aeff2cb44301ab2fb6e26289
Seasonbrucelee <sezen39@hotmail.com>
2022-02-18 10:27:32 +0100

    modif du 18.02.22

---
+++ originedanse/src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+   <img src="@/assets/logo_texte.png" alt="logo" width="150">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Cours Open
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Krump débutant</a></li>
+            <li><a class="dropdown-item" href="#">Krump intermédiare</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Événements</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Professeurs</a>
+        </li>
+
+       <li class="nav-item">
+          <a class="nav-link" href="#">Formations</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Chercher" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Chercher</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+   // HelloWorld
+  }
+}
+</script>
